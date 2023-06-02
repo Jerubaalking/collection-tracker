1aad22f4b25900b176735bfc4b85770c371f4348
Gideon Sainyeye <gsainyeye@gmail.com>
2023-06-02 15:16:01 +0300

    first commit

--- main-https.js
+++ main-https.js
@@ -13,7 +13,6 @@ const LocalStrategy = require('passport-local').Strategy;
 const app = require('./applications/app');
 
 dotenv.config({ path: './config/config.env' });
-const cors = require('cors');
 
 const models = require("./database/models/module_exporter");
 app.use(cors({
